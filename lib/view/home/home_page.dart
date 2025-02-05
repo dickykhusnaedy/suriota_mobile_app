a8836657807d3a441f5bc78053e203ecdabf15ce
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-06 00:13:23 +0700

    Refactor HomePage to use StatefulWidget and improve UI layout with SafeArea and custom floating action button

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,63 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:suriota_mobile_gateway/global/widgets/device_card.dart';
 import 'package:suriota_mobile_gateway/models/device_dummy.dart';
 import 'package:suriota_mobile_gateway/models/device_model.dart';
 import 'package:suriota_mobile_gateway/view/device_menu/device_menu.dart';
-import 'package:suriota_mobile_gateway/global/widgets/device_card.dart';
 import 'package:suriota_mobile_gateway/view/sidebar_menu/sidebar_menu.dart';
+
 import '../../constant/app_color.dart';
 import '../../constant/font_setup.dart';
 import '../../constant/image_asset.dart';
 import 'add_device_page.dart';
-// ignore: must_be_immutable
-class HomePage extends StatelessWidget {
-  List<DeviceModel> deviceList = deviceDummy;
 
-  HomePage({super.key});
+// ignore: must_be_immutable
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<DeviceModel> deviceList = deviceDummy;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Transform.scale(
-            alignment: Alignment.centerLeft,
-            scale: 0.5,
-            child: Image.asset(ImageAsset.logoSuriota)),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          ImageAsset.logoSuriota,
+          width: 161,
+          height: 38,
+          fit: BoxFit.contain,
+        ),
       ),
       endDrawer: const SideBarMenu(),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Hallo, Soru👋',
-              style: FontFamily.titleLarge,
-            ),
-            Text(
-              'Connecting the device near you',
-              style: FontFamily.normal.copyWith(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Text(
-              'Device List',
-              style: FontFamily.headlineMedium,
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: deviceList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>DeviceMenuConfigurationPage(
-                          title: 'Suriota Gateway ${index + 1}',
-                        )));
-                  },
-                  child: DeviceCard(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hallo, Soru👋',
+                style: FontFamily.titleLarge,
+              ),
+              Text(
+                'Connecting the device near you',
+                style: FontFamily.normal.copyWith(fontSize: 18),
+              ),
+              const SizedBox(height: 32),
+              Text(
+                'Device List',
+                style: FontFamily.headlineMedium,
+              ),
+              const SizedBox(height: 10),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: deviceList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    DeviceMenuConfigurationPage(
+                                      title: 'Suriota Gateway ${index + 1}',
+                                    )));
+                      },
+                      child: DeviceCard(
                         deviceTitle: deviceList[index].deviceTitle,
                         deviceAddress: deviceList[index].deviceAddress,
                         buttonTitle: deviceList[index].isConnected
@@ -67,26 +80,47 @@ class HomePage extends StatelessWidget {
                             ? AppColor.redColor
                             : AppColor.primaryColor,
                         onPressed: () {},
-                      )
-                );
-              },
-            ),
-          ],
+                      ));
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: AppColor.primaryColor,
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const AddDevicePage()));
-        },
-        shape: (const CircleBorder()),
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-          size: 25,
-        ),
-      ),
+      floatingActionButton: _floatingButtonCustom(context),
     );
+  }
+
+  Container _floatingButtonCustom(BuildContext context) {
+    return Container(
+        height: 50,
+        width: 130,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+        decoration: BoxDecoration(
+          color: AppColor.primaryColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const AddDevicePage()));
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.add_circle,
+                size: 20,
+                color: Colors.white,
+              ),
+              const SizedBox(width: 10),
+              Text(
+                'Add Device',
+                style: FontFamily.titleMedium
+                    .copyWith(color: Colors.white, fontSize: 14),
+              ),
+            ],
+          ),
+        ));
   }
 }
