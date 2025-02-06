fe57317dc0323d60f4ffb859a07973516db1715a
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-07 00:41:09 +0700

    Add AppSpacing and AppPadding classes for consistent spacing and padding throughout the application

--- lib/constant/app_gap.dart
+++ lib/constant/app_gap.dart
@@ -1,4 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
+
+class AppSpacing {
+  static final xs = Gap(4.sp);
+  static final sm = Gap(8.sp);
+  static final md = Gap(16.sp);
+  static final lg = Gap(24.sp);
+  static final xl = Gap(32.sp);
+  static final xxl = Gap(48.sp);
+  static final xxxl = Gap(55.sp);
+  static final xxxxl = Gap(80.sp);
+}
+
+class AppPadding {
+  static EdgeInsets small = EdgeInsets.all(8.w);
+  static EdgeInsets medium = EdgeInsets.all(16.w);
+  static EdgeInsets large = EdgeInsets.all(24.w);
+
+  static EdgeInsets horizontalSmall = EdgeInsets.symmetric(horizontal: 8.w);
+  static EdgeInsets horizontalMedium = EdgeInsets.symmetric(horizontal: 16.w);
+  static EdgeInsets horizontalLarge = EdgeInsets.symmetric(horizontal: 24.w);
+
+  static EdgeInsets verticalSmall = EdgeInsets.symmetric(vertical: 8.h);
+  static EdgeInsets verticalMedium = EdgeInsets.symmetric(vertical: 16.h);
+  static EdgeInsets verticalLarge = EdgeInsets.symmetric(vertical: 24.h);
+
+  static EdgeInsets cardPadding =
+      EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h);
+
+  static EdgeInsets screenPadding = EdgeInsets.all(16.w);
+}
 
 class AppGap {
   AppGap._();
