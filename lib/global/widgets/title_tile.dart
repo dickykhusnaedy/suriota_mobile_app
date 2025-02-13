6ed6e1851f23a81b41b0038bfc526a44dabecd6f
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-13 22:20:20 +0700

    Refactor TitleTile to enhance styling and utilize context extensions for padding and text styles

--- lib/global/widgets/title_tile.dart
+++ lib/global/widgets/title_tile.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
-
-import '../../constant/app_color.dart';
-import '../../constant/font_setup.dart';
+import 'package:suriota_mobile_gateway/constant/app_color.dart';
+import 'package:suriota_mobile_gateway/constant/app_gap.dart';
+import 'package:suriota_mobile_gateway/global/utils/text_extension.dart';
 
 class TitleTile extends StatelessWidget {
   final String title;
   final Color? bgColor;
+
   const TitleTile({
     super.key,
-    required this.title,  this.bgColor ,
+    required this.title,
+    this.bgColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+      width: double.infinity,
+      padding: AppPadding.small,
       decoration: BoxDecoration(
-          color: bgColor ??  AppColor.primaryColor.withOpacity(0.2),
+          color: bgColor ?? AppColor.primaryColor.withValues(alpha: 0.2),
           borderRadius: BorderRadius.circular(8)),
       child: Text(
         title,
-        style: FontFamily.headlineMedium,
+        style: context.h6,
       ),
     );
   }
