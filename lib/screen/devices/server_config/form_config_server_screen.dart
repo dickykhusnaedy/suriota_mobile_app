cfae5cf2d1bab46f85d20554aac0ea39d4e63d14
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-03-19 22:04:18 +0700

    Enhance FormConfigServer with data interval selection and protocol options

--- lib/screen/devices/server_config/form_config_server_screen.dart
+++ lib/screen/devices/server_config/form_config_server_screen.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:multi_dropdown/multi_dropdown.dart';
 import 'package:suriota_mobile_gateway/constant/app_color.dart';
 import 'package:suriota_mobile_gateway/constant/app_gap.dart';
+import 'package:suriota_mobile_gateway/constant/font_setup.dart';
 import 'package:suriota_mobile_gateway/constant/theme.dart';
 import 'package:suriota_mobile_gateway/global/utils/text_extension.dart';
 import 'package:suriota_mobile_gateway/global/widgets/custom_button.dart';
+import 'package:suriota_mobile_gateway/global/widgets/custom_dropdown.dart';
 import 'package:suriota_mobile_gateway/global/widgets/custom_radiotile.dart';
 import 'package:suriota_mobile_gateway/global/widgets/custom_textfield.dart';
 import 'package:suriota_mobile_gateway/global/widgets/title_tile.dart';
@@ -17,93 +20,208 @@ class FormConfigServer extends StatefulWidget {
   State<FormConfigServer> createState() => _FormConfigServerState();
 }
 
+class LoggingData {
+  final String name;
+  final int id;
+
+  LoggingData({required this.name, required this.id});
+
+  @override
+  String toString() {
+    return 'LoggingData(name: $name, id: $id)';
+  }
+}
+
 class _FormConfigServerState extends State<FormConfigServer> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   String protocolSelected = 'MQTT';
   String communcationSelected = 'Ethernet';
   String methodEthernet = "Automatic";
+  String loggingIntervalSelected = "30 seconds";
+
   String? confirmAuthentication;
 
   @override
   Widget build(BuildContext context) {
+    var items = [
+      DropdownItem(label: 'Sensor', value: LoggingData(name: 'Sensor', id: 1)),
+      DropdownItem(
+          label: 'Tekanan', value: LoggingData(name: 'Tekanan', id: 6)),
+      DropdownItem(label: 'Suhu', value: LoggingData(name: 'Suhu', id: 2)),
+    ];
+
+    List<String> typeInterval = [
+      'Seconds',
+      'Minutes',
+    ];
+
     return Scaffold(
       appBar: _appBar(context),
       body: SafeArea(
         child: SingleChildScrollView(
           padding: AppPadding.horizontalMedium,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppSpacing.md,
-              Column(
-                children: [
-                  const TitleTile(title: 'Choose Communication Mode'),
-                  AppSpacing.sm,
-                  // Ethernet Option
-                  CustomRadioTile(
-                    value: "Ethernet",
-                    grupValue:
-                        communcationSelected, // Use dynamic state variable
-                    onChanges: () {
-                      setState(() {
-                        communcationSelected =
-                            "Ethernet"; // Set Ethernet as selected communication
-                      });
-                      _chooseEthernetMethod(context);
-                    },
-                  ),
-                  // WiFi Option
-                  CustomRadioTile(
-                    value: "WiFi",
-                    grupValue:
-                        communcationSelected, // Use dynamic state variable
-                    onChanges: () {
-                      setState(() {
-                        communcationSelected =
-                            "WiFi"; // Set WiFi as selected communication
-                      });
-                    },
-                  ),
-                ],
-              ),
-              AppSpacing.md,
-              // Show Ethernet Following fields if selected
-              if (methodEthernet == "Following") ethernetFollowing(),
-              if (communcationSelected == "WiFi") wifiField(),
-              Column(
-                children: [
-                  const TitleTile(title: 'Choose Protocol'),
-                  AppSpacing.sm,
-                  CustomRadioTile(
-                    value: "MQTT",
-                    grupValue: protocolSelected,
-                    onChanges: () {
-                      if (protocolSelected != "MQTT") {
-                        setState(() {
-                          protocolSelected = "MQTT";
-                        });
-                      }
-                    },
-                  ),
-                  CustomRadioTile(
-                    value: "HTTP",
-                    grupValue: protocolSelected,
-                    onChanges: () {
-                      if (protocolSelected != "HTTP") {
-                        setState(() {
-                          protocolSelected = "HTTP";
-                        });
-                      }
-                    },
-                  ),
-                ],
-              ),
-              AppSpacing.md,
-              if (protocolSelected == "MQTT") mqttField(),
-              if (protocolSelected == "HTTP") httpField(),
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppSpacing.md,
+                _communicationModeWrapper(context),
+                AppSpacing.md,
+                // Show Ethernet Following fields if selected
+                if (methodEthernet == "Following") ethernetFollowing(),
+                if (communcationSelected == "WiFi") wifiField(),
+                _protocolWrapper(),
+                AppSpacing.md,
+                _intervalWrapper(context, items, typeInterval),
+                AppSpacing.md,
+                if (protocolSelected == "MQTT") mqttField(),
+                if (protocolSelected == "HTTP") httpField(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Column _intervalWrapper(BuildContext context,
+      List<DropdownItem<LoggingData>> items, List<String> typeInterval) {
+    return Column(children: [
+      TitleTile(title: 'Data Interval'),
+      AppSpacing.sm,
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Data Interval',
+              style: context.h6.copyWith(fontWeight: FontWeightTheme.bold)),
+          AppSpacing.sm,
+          MultiDropdown<LoggingData>(
+            items: items,
+            enabled: true,
+            chipDecoration: ChipDecoration(
+                labelStyle: context.buttonTextSmall
+                    .copyWith(color: AppColor.whiteColor),
+                backgroundColor: AppColor.primaryColor,
+                padding: AppPadding.small,
+                wrap: true,
+                runSpacing: 10,
+                spacing: 5,
+                deleteIcon: const Icon(
+                  Icons.cancel,
+                  size: 17,
+                  color: AppColor.whiteColor,
+                )),
+            fieldDecoration: FieldDecoration(
+              hintText: 'Choose Data Interval',
+              hintStyle: context.body,
+              showClearIcon: false,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide:
+                    const BorderSide(color: AppColor.primaryColor, width: 1),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide:
+                    const BorderSide(color: AppColor.primaryColor, width: 2),
+              ),
+            ),
+            dropdownItemDecoration: const DropdownItemDecoration(
+              textColor: AppColor.grey,
+              selectedTextColor: AppColor.primaryColor,
+              selectedIcon: Icon(Icons.check_box, color: AppColor.primaryColor),
+              disabledIcon: Icon(Icons.lock, color: Colors.grey),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please select a data interval';
+              }
+              return null;
+            },
+            onSelectionChange: (selectedItems) {
+              debugPrint("OnSelectionChange: $selectedItems");
+            },
+          ),
+        ],
+      ),
+      AppSpacing.md,
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Interval Time',
+              style: context.h6.copyWith(fontWeight: FontWeightTheme.bold)),
+          CustomTextFormField(
+              hintTxt: 'Set Interval Time', keyboardType: TextInputType.number),
+          AppSpacing.sm,
+          CustomDropdown(
+              listItem: typeInterval, hintText: 'Choose Interval Type'),
+        ],
+      )
+    ]);
+  }
+
+  Column _protocolWrapper() {
+    return Column(
+      children: [
+        const TitleTile(title: 'Choose Protocol'),
+        AppSpacing.sm,
+        CustomRadioTile(
+          value: "MQTT",
+          grupValue: protocolSelected,
+          onChanges: () {
+            if (protocolSelected != "MQTT") {
+              setState(() {
+                protocolSelected = "MQTT";
+              });
+            }
+          },
+        ),
+        CustomRadioTile(
+          value: "HTTP",
+          grupValue: protocolSelected,
+          onChanges: () {
+            if (protocolSelected != "HTTP") {
+              setState(() {
+                protocolSelected = "HTTP";
+              });
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  Column _communicationModeWrapper(BuildContext context) {
+    return Column(
+      children: [
+        const TitleTile(title: 'Choose Communication Mode'),
+        AppSpacing.sm,
+        // Ethernet Option
+        CustomRadioTile(
+          value: "Ethernet",
+          grupValue: communcationSelected, // Use dynamic state variable
+          onChanges: () {
+            setState(() {
+              communcationSelected =
+                  "Ethernet"; // Set Ethernet as selected communication
+            });
+            _chooseEthernetMethod(context);
+          },
+        ),
+        // WiFi Option
+        CustomRadioTile(
+          value: "WiFi",
+          grupValue: communcationSelected, // Use dynamic state variable
+          onChanges: () {
+            setState(() {
+              communcationSelected =
+                  "WiFi"; // Set WiFi as selected communication
+            });
+          },
+        ),
+      ],
     );
   }
 
@@ -212,6 +330,11 @@ class _FormConfigServerState extends State<FormConfigServer> {
         CustomTextFormField(
           labelTxt: "Port MQTT",
           hintTxt: "Enter the Port MQTT",
+        ),
+        AppSpacing.md,
+        CustomTextFormField(
+          labelTxt: "Publish Topic",
+          hintTxt: "Enter the Publish Topic",
         ),
         AppSpacing.md,
         CustomTextFormField(
