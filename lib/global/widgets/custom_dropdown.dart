130be1da31711a574eb108c75665ef3542f50f68
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-05-07 00:10:19 +0700

    Refactor CustomDropdown: Simplify state management and enhance dropdown properties for improved readability and maintainability

--- lib/global/widgets/custom_dropdown.dart
+++ lib/global/widgets/custom_dropdown.dart
@@ -23,119 +23,103 @@ class CustomDropdown extends StatefulWidget {
 }
 
 class _CustomDropdownState extends State<CustomDropdown> {
-  String? selectedBaudrate;
+  String? initialSelect;
 
   @override
   void initState() {
     super.initState();
-    // Set nilai awal dari selectedBaudrate yang diterima dari widget
-    selectedBaudrate = widget.selectedItem;
+    // Set nilai awal dari initialSelect yang diterima dari widget
+    initialSelect = widget.selectedItem;
   }
 
   @override
   Widget build(BuildContext context) {
     return DropdownSearch<String>(
       items: widget.listItem,
-      dropdownDecoratorProps: DropDownDecoratorProps(
-        baseStyle: context.h6,
-        dropdownSearchDecoration: InputDecoration(
-          hintText: widget.hintText,
-          hintStyle: context.body.copyWith(color: AppColor.grey),
-          filled: true,
-          fillColor: Colors.white, // Sama dengan CustomTextFormField
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: const BorderSide(
-              color: AppColor.primaryColor,
-              width: 1,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: const BorderSide(
-              color: AppColor.primaryColor,
-              width: 2,
-            ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: const BorderSide(
-              color: AppColor.primaryColor,
-              width: 1,
-            ),
-          ),
-          contentPadding: AppPadding.horizontalMedium,
-        ),
-      ),
+      dropdownDecoratorProps: _buildDropdownDecoratorProps(context),
       onChanged: widget.onChanged,
-      selectedItem:
-          selectedBaudrate?.isNotEmpty == true ? selectedBaudrate : null,
-      // Mengatur props popup berdasarkan jumlah item
-      popupProps: widget.listItem.length > 5
-          ? PopupProps.dialog(
-              fit: FlexFit.loose,
-              dialogProps: const DialogProps(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
-              ),
-              showSelectedItems: true,
-              showSearchBox: true,
-              searchFieldProps: TextFieldProps(
-                style: context.body,
-                textCapitalization: TextCapitalization.characters,
-                decoration: InputDecoration(
-                    contentPadding: AppPadding.medium,
-                    enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide:
-                            BorderSide(color: AppColor.primaryColor, width: 1)),
-                    focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide:
-                            BorderSide(color: AppColor.primaryColor, width: 2)),
-                    hintText: "Search",
-                    hintStyle: context.body),
-              ),
-              itemBuilder: (context, item, isSelected) {
-                return Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: isSelected
-                        ? AppColor.primaryColor.withValues(alpha: 0.1)
-                        : null,
-                  ),
-                  child: Text(
-                    item,
-                    style: context.body,
-                  ),
-                );
-              },
-            )
-          : PopupProps.menu(
-              fit: FlexFit.loose,
-              menuProps: const MenuProps(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(4)))),
-              showSelectedItems: true,
-              showSearchBox: false,
-              itemBuilder: (context, item, isSelected) {
-                return Container(
-                  padding: AppPadding.medium,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: isSelected
-                        ? AppColor.primaryColor.withValues(alpha: 0.1)
-                        : null,
-                  ),
-                  child: Text(
-                    item,
-                    style: context.body.copyWith(color: AppColor.primaryColor),
-                  ),
-                );
-              },
+      selectedItem: initialSelect?.isNotEmpty == true ? initialSelect : null,
+      popupProps: _buildPopupProps(context),
+    );
+  }
+
+  DropDownDecoratorProps _buildDropdownDecoratorProps(BuildContext context) {
+    return DropDownDecoratorProps(
+      baseStyle: context.h6,
+      dropdownSearchDecoration: InputDecoration(
+        hintText: widget.hintText,
+        hintStyle: context.body.copyWith(color: AppColor.grey),
+        filled: true,
+        fillColor: Colors.white,
+        enabledBorder: _buildOutlineInputBorder(AppColor.primaryColor, 1),
+        focusedBorder: _buildOutlineInputBorder(AppColor.primaryColor, 2),
+        border: _buildOutlineInputBorder(AppColor.primaryColor, 1),
+        contentPadding: AppPadding.horizontalMedium,
+      ),
+    );
+  }
+
+  PopupProps<String> _buildPopupProps(BuildContext context) {
+    return widget.listItem.length > 5
+        ? PopupProps.dialog(
+            fit: FlexFit.loose,
+            dialogProps: const DialogProps(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
             ),
+            showSelectedItems: true,
+            showSearchBox: true,
+            searchFieldProps: _buildSearchFieldProps(context),
+            itemBuilder: (context, item, isSelected) =>
+                _buildPopupItem(context, item, isSelected),
+          )
+        : PopupProps.menu(
+            fit: FlexFit.loose,
+            menuProps: const MenuProps(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4)))),
+            showSelectedItems: true,
+            showSearchBox: false,
+            itemBuilder: (context, item, isSelected) =>
+                _buildPopupItem(context, item, isSelected),
+          );
+  }
+
+  TextFieldProps _buildSearchFieldProps(BuildContext context) {
+    return TextFieldProps(
+      style: context.body,
+      textCapitalization: TextCapitalization.characters,
+      decoration: InputDecoration(
+        contentPadding: AppPadding.medium,
+        enabledBorder: _buildOutlineInputBorder(AppColor.primaryColor, 1),
+        focusedBorder: _buildOutlineInputBorder(AppColor.primaryColor, 2),
+        hintText: "Search",
+        hintStyle: context.body,
+      ),
+    );
+  }
+
+  Widget _buildPopupItem(BuildContext context, String item, bool isSelected) {
+    return Container(
+      padding: AppPadding.medium,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: isSelected ? AppColor.primaryColor.withValues(alpha: 0.1) : null,
+      ),
+      child: Text(
+        item,
+        style: context.body,
+      ),
+    );
+  }
+
+  OutlineInputBorder _buildOutlineInputBorder(Color color, double width) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(
+        color: color,
+        width: width,
+      ),
     );
   }
 }
