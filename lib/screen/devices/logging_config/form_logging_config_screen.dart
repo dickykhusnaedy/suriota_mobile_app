ad0e1ac30a83d8f8947f9d28ef755e8046253a64
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-05-29 00:08:18 +0700

    refactor: enhance form logging configuration with improved validation and BLE connection checks

--- lib/screen/devices/logging_config/form_logging_config_screen.dart
+++ lib/screen/devices/logging_config/form_logging_config_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:multi_dropdown/multi_dropdown.dart';
+import 'package:get/get.dart';
 import 'package:suriota_mobile_gateway/constant/app_color.dart';
 import 'package:suriota_mobile_gateway/constant/app_gap.dart';
-import 'package:suriota_mobile_gateway/constant/font_setup.dart';
-import 'package:suriota_mobile_gateway/constant/theme.dart';
+import 'package:suriota_mobile_gateway/controller/ble_controller.dart';
 import 'package:suriota_mobile_gateway/global/utils/text_extension.dart';
+import 'package:suriota_mobile_gateway/global/widgets/custom_alert_dialog.dart';
 import 'package:suriota_mobile_gateway/global/widgets/custom_button.dart';
 import 'package:suriota_mobile_gateway/global/widgets/custom_radiotile.dart';
+import 'package:suriota_mobile_gateway/global/widgets/loading_overlay.dart';
 import 'package:suriota_mobile_gateway/global/widgets/title_tile.dart';
 
 class FormLoggingConfigScreen extends StatefulWidget {
@@ -30,180 +31,92 @@ class LoggingData {
 }
 
 class _FormLoggingConfigScreenState extends State<FormLoggingConfigScreen> {
+  final BLEController bleController = Get.put(BLEController(), permanent: true);
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  String protocolSelected = 'MQTT';
-  String loggingRetentionSelected =
-      "1 Week"; // Variabel state baru untuk Retensi
-  String loggingIntervalSelected =
-      "5 Minutes"; // Variabel state baru untuk Interval
+  String loggingRetentionSelected = "";
+  String loggingIntervalSelected = "";
+
+  void backNTimes(int n) {
+    if (n <= 0) {
+      return;
+    }
+    int count = 0;
+    Get.until((route) {
+      return count++ == n;
+    });
+  }
+
+  void _submit() {
+    if (loggingIntervalSelected.isEmpty || loggingRetentionSelected.isEmpty) {
+      Get.snackbar(
+        '',
+        'Please select both logging retention and interval',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: AppColor.redColor,
+        colorText: AppColor.whiteColor,
+        duration: const Duration(seconds: 3),
+        margin: const EdgeInsets.all(16),
+        titleText: const SizedBox(),
+        padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
+      );
+      return;
+    }
+
+    // Periksa koneksi BLE
+    if (bleController.isConnected.isEmpty ||
+        !bleController.isConnected.values.any((connected) => connected)) {
+      Get.snackbar('Error', 'No BLE device connected');
+      return;
+    }
+
+    CustomAlertDialog.show(
+      title: "Are you sure?",
+      message: "Are you sure you want to save this logging config?",
+      primaryButtonText: 'Yes',
+      secondaryButtonText: 'No',
+      onPrimaryPressed: () async {
+        Get.back();
+        await Future.delayed(const Duration(seconds: 1));
+
+        try {
+          final sendDataDelimiter =
+              'UPDATE|logging_config|retention:$loggingRetentionSelected|interval:$loggingIntervalSelected';
+          bleController.sendCommand(sendDataDelimiter, 'logging_config');
+        } catch (e) {
+          debugPrint('Error submitting form: $e');
+          Get.snackbar('Error', 'Failed to submit form: $e');
+        } finally {
+          await Future.delayed(const Duration(seconds: 3));
+          backNTimes(1);
+        }
+      },
+      barrierDismissible: false,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var items = [
-      DropdownItem(label: 'Sensor', value: LoggingData(name: 'Sensor', id: 1)),
-      DropdownItem(
-          label: 'Tekanan', value: LoggingData(name: 'Tekanan', id: 6)),
-      DropdownItem(label: 'Suhu', value: LoggingData(name: 'Suhu', id: 2)),
-    ];
-
-    return Scaffold(
-      appBar: _appBar(context),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: AppPadding.horizontalMedium,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppSpacing.md,
-              Text(
-                'Data Interval',
-                style: context.h6,
-              ),
-              Form(
-                key: _formKey, // Mengaitkan GlobalKey ke form
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    AppSpacing.sm,
-                    MultiDropdown<LoggingData>(
-                      items: items,
-                      enabled: true,
-                      // searchEnabled: true,
-                      chipDecoration: ChipDecoration(
-                          labelStyle: context.buttonTextSmall
-                              .copyWith(color: AppColor.whiteColor),
-                          backgroundColor: AppColor.primaryColor,
-                          padding: AppPadding.small,
-                          wrap: true,
-                          runSpacing: 10,
-                          spacing: 5,
-                          deleteIcon: const Icon(
-                            Icons.cancel,
-                            size: 17,
-                            color: AppColor.whiteColor,
-                          )),
-                      fieldDecoration: FieldDecoration(
-                        hintText: 'Choose Data Interval',
-                        hintStyle: FontFamily.labelText,
-                        showClearIcon: false,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(
-                              color: AppColor.primaryColor, width: 2),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(
-                              color: AppColor.primaryColor, width: 2),
-                        ),
-                      ),
-                      dropdownItemDecoration: const DropdownItemDecoration(
-                        textColor: AppColor.grey,
-                        selectedTextColor: AppColor.primaryColor,
-                        selectedIcon:
-                            Icon(Icons.check_box, color: AppColor.primaryColor),
-                        disabledIcon: Icon(Icons.lock, color: Colors.grey),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please select a daata interval';
-                        }
-                        return null;
-                      },
-                      onSelectionChange: (selectedItems) {
-                        debugPrint("OnSelectionChange: $selectedItems");
-                      },
-                    ),
-                    AppSpacing.md,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const TitleTile(title: 'Choose Logging Retention'),
-                        AppSpacing.sm,
-                        CustomRadioTile(
-                          value: "1 Week",
-                          grupValue: loggingRetentionSelected,
-                          onChanges: () {
-                            setState(() {
-                              loggingRetentionSelected = "1 Week";
-                            });
-                          },
-                        ),
-                        CustomRadioTile(
-                          value: "1 Month",
-                          grupValue: loggingRetentionSelected,
-                          onChanges: () {
-                            setState(() {
-                              loggingRetentionSelected = "1 Month";
-                            });
-                          },
-                        ),
-                        CustomRadioTile(
-                          value: "3 Month",
-                          grupValue: loggingRetentionSelected,
-                          onChanges: () {
-                            setState(() {
-                              loggingRetentionSelected = "3 Month";
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    AppSpacing.md,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const TitleTile(title: 'Choose Logging Interval'),
-                        AppSpacing.sm,
-                        CustomRadioTile(
-                          value: "5 Minutes",
-                          grupValue: loggingIntervalSelected,
-                          onChanges: () {
-                            setState(() {
-                              loggingIntervalSelected = "5 Minutes";
-                            });
-                          },
-                        ),
-                        CustomRadioTile(
-                          value: "10 Minutes",
-                          grupValue: loggingIntervalSelected,
-                          onChanges: () {
-                            setState(() {
-                              loggingIntervalSelected = "10 Minutes";
-                            });
-                          },
-                        ),
-                        CustomRadioTile(
-                          value: "30 Minutes",
-                          grupValue: loggingIntervalSelected,
-                          onChanges: () {
-                            setState(() {
-                              loggingIntervalSelected = "30 Minutes";
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    AppSpacing.lg,
-                    Button(
-                      width: MediaQuery.of(context).size.width,
-                      onPressed: () {
-                        ShowMessage.showCustomSnackBar(
-                            context, "Feature for save data is coming soon!");
-                      },
-                      text: 'Save',
-                      height: 50,
-                    ),
-                    AppSpacing.lg,
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: _appBar(context),
+          body: _body(context),
         ),
-      ),
+        Obx(() {
+          final isAnyDeviceLoading = bleController.isLoading.value;
+          return LoadingOverlay(
+            isLoading: isAnyDeviceLoading,
+            message: 'Processing request...',
+          );
+        }),
+      ],
     );
   }
 
@@ -214,6 +127,164 @@ class _FormLoggingConfigScreenState extends State<FormLoggingConfigScreen> {
       iconTheme: const IconThemeData(color: AppColor.whiteColor),
       backgroundColor: AppColor.primaryColor,
       centerTitle: true,
+    );
+  }
+
+  SafeArea _body(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: AppPadding.horizontalMedium,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppSpacing.md,
+            Form(
+              key: _formKey, // Mengaitkan GlobalKey ke form
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  // Text(
+                  //   'Data Interval',
+                  //   style: context.h6,
+                  // ),
+                  // AppSpacing.sm,
+                  // MultiDropdown<LoggingData>(
+                  //   items: items,
+                  //   enabled: true,
+                  //   // searchEnabled: true,
+                  //   chipDecoration: ChipDecoration(
+                  //       labelStyle: context.buttonTextSmall
+                  //           .copyWith(color: AppColor.whiteColor),
+                  //       backgroundColor: AppColor.primaryColor,
+                  //       padding: AppPadding.small,
+                  //       wrap: true,
+                  //       runSpacing: 10,
+                  //       spacing: 5,
+                  //       deleteIcon: const Icon(
+                  //         Icons.cancel,
+                  //         size: 17,
+                  //         color: AppColor.whiteColor,
+                  //       )),
+                  //   fieldDecoration: FieldDecoration(
+                  //     hintText: 'Choose Data Interval',
+                  //     hintStyle: FontFamily.labelText,
+                  //     showClearIcon: false,
+                  //     border: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(12),
+                  //       borderSide: const BorderSide(
+                  //           color: AppColor.primaryColor, width: 2),
+                  //     ),
+                  //     focusedBorder: OutlineInputBorder(
+                  //       borderRadius: BorderRadius.circular(12),
+                  //       borderSide: const BorderSide(
+                  //           color: AppColor.primaryColor, width: 2),
+                  //     ),
+                  //   ),
+                  //   dropdownItemDecoration: const DropdownItemDecoration(
+                  //     textColor: AppColor.grey,
+                  //     selectedTextColor: AppColor.primaryColor,
+                  //     selectedIcon:
+                  //         Icon(Icons.check_box, color: AppColor.primaryColor),
+                  //     disabledIcon: Icon(Icons.lock, color: Colors.grey),
+                  //   ),
+                  //   validator: (value) {
+                  //     if (value == null || value.isEmpty) {
+                  //       return 'Please select a daata interval';
+                  //     }
+                  //     return null;
+                  //   },
+                  //   onSelectionChange: (selectedItems) {
+                  //     debugPrint("OnSelectionChange: $selectedItems");
+                  //   },
+                  // ),
+                  // AppSpacing.md,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const TitleTile(
+                          title:
+                              'Choose Logging Retention (w: week, m: month)'),
+                      AppSpacing.sm,
+                      CustomRadioTile(
+                        value: "1w",
+                        grupValue: loggingRetentionSelected,
+                        onChanges: () {
+                          setState(() {
+                            loggingRetentionSelected = "1w";
+                          });
+                        },
+                      ),
+                      CustomRadioTile(
+                        value: "1m",
+                        grupValue: loggingRetentionSelected,
+                        onChanges: () {
+                          setState(() {
+                            loggingRetentionSelected = "1m";
+                          });
+                        },
+                      ),
+                      CustomRadioTile(
+                        value: "3m",
+                        grupValue: loggingRetentionSelected,
+                        onChanges: () {
+                          setState(() {
+                            loggingRetentionSelected = "3m";
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  AppSpacing.md,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const TitleTile(
+                          title: 'Choose Logging Interval (m: minute)'),
+                      AppSpacing.sm,
+                      CustomRadioTile(
+                        value: "5m",
+                        grupValue: loggingIntervalSelected,
+                        onChanges: () {
+                          setState(() {
+                            loggingIntervalSelected = "5m";
+                          });
+                        },
+                      ),
+                      CustomRadioTile(
+                        value: "10m",
+                        grupValue: loggingIntervalSelected,
+                        onChanges: () {
+                          setState(() {
+                            loggingIntervalSelected = "10m";
+                          });
+                        },
+                      ),
+                      CustomRadioTile(
+                        value: "30m",
+                        grupValue: loggingIntervalSelected,
+                        onChanges: () {
+                          setState(() {
+                            loggingIntervalSelected = "30m";
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  AppSpacing.lg,
+                  Button(
+                    width: MediaQuery.of(context).size.width,
+                    onPressed: _submit,
+                    text: 'Save',
+                    height: 50,
+                  ),
+                  AppSpacing.lg,
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
