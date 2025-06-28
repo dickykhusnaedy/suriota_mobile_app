a9f3d72439d4357ed9c725afb21f8f4a143b762c
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-06-28 12:37:14 +0700

    feat: add launchInBrowser method to open URLs in external browser

--- lib/global/utils/helper.dart
+++ lib/global/utils/helper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppHelpers {
   static String debugLog(String message) {
@@ -19,5 +20,14 @@ class AppHelpers {
     Get.until((route) {
       return count++ == n;
     });
+  }
+
+  static Future<void> launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
