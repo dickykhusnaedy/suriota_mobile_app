b7efac7aef62e0efd38fc506cbd562a1a681a477
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-09-10 00:07:17 +0700

    refactor: update splash screen layout and improve status bar handling

--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gateway_config/core/constants/app_gap.dart';
 import 'package:gateway_config/core/constants/app_image_assets.dart';
 import 'package:gateway_config/core/services/bluetooth/bluetooth_permission_service.dart';
@@ -28,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     bool permissionGranted =
         await BluetoothPermissionService.checkAndRequestPermissions(context);
-        
+
     if (mounted) {
       if (permissionGranted) {
         context.go('/');
@@ -41,26 +42,32 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Image.asset(
-                    ImageAsset.logoSuriota, // Ganti dengan path logo Anda
-                    width: 200,
-                    height: 200,
-                  ),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: Colors.white, // Latar belakang status bar transparan
+          statusBarIconBrightness:
+              Brightness.dark, // Warna ikon status bar (misal: putih)
+          statusBarBrightness: Brightness.dark, // Kecerahan status bar
+          systemNavigationBarColor: Colors.black, // Warna navigasi bar
+          systemNavigationBarIconBrightness: Brightness.dark,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Image.asset(
+                  ImageAsset.logoSuriota, // Ganti dengan path logo Anda
+                  width: 200,
+                  height: 200,
                 ),
-                Text("v1.0.0", style: context.bodySmall),
-                AppSpacing.lg,
-              ],
-            ),
+              ),
+              Text("v1.0.0", style: context.bodySmall),
+              AppSpacing.lg,
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
