11b7399fda31719d00778a3659a335aaab8e166c
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-10-09 01:41:01 +0700

    refactor(controller): update server controller

--- lib/core/controllers/server_config_controller.dart
+++ lib/core/controllers/server_config_controller.dart
@@ -1,8 +1,61 @@
+import 'package:gateway_config/core/constants/app_color.dart';
 import 'package:gateway_config/core/controllers/ble_controller.dart';
+import 'package:gateway_config/core/utils/app_helpers.dart';
+import 'package:gateway_config/core/utils/snackbar_custom.dart';
+import 'package:gateway_config/models/device_model.dart';
 import 'package:get/get.dart';
 
 class ServerConfigController extends GetxController {
+  final RxList<Map<String, dynamic>> dataServer = <Map<String, dynamic>>[].obs;
   final RxBool isFetching = false.obs;
 
   final BleController bleController = Get.put(BleController());
+
+  Future<void> fetchDevices(DeviceModel model) async {
+    isFetching.value = true;
+
+    try {
+      if (!model.isConnected.value) {
+        SnackbarCustom.showSnackbar(
+          '',
+          'Device not connected',
+          AppColor.redColor,
+          AppColor.whiteColor,
+        );
+        return;
+      }
+
+      final response = await bleController.readCommandResponse(
+        model,
+        type: 'server_config',
+      );
+
+      if (response.status == 'ok' || response.status == 'success') {
+        dataServer.assignAll(
+          (response.config as List<dynamic>?)?.cast<Map<String, dynamic>>() ??
+              [],
+        );
+      } else {
+        SnackbarCustom.showSnackbar(
+          '',
+          response.message ?? 'Failed to fecth devices',
+          AppColor.redColor,
+          AppColor.whiteColor,
+        );
+
+        dataServer.clear();
+      }
+    } catch (e) {
+      AppHelpers.debugLog('Error fetching server: $e');
+      SnackbarCustom.showSnackbar(
+        'Error',
+        'Failed to fetch devices',
+        AppColor.redColor,
+        AppColor.whiteColor,
+      );
+      dataServer.clear();
+    } finally {
+      isFetching.value = false;
+    }
+  }
 }
