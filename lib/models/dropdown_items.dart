9cf1046c17fdc9ff90ba2ae0920ca601102def24
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-10-26 00:46:25 +0700

    feat(dropdown): add optional group field to DropdownItems class

--- lib/models/dropdown_items.dart
+++ lib/models/dropdown_items.dart
@@ -1,6 +1,7 @@
 class DropdownItems {
   final String text;
   final String value;
+  final String? group;
 
-  DropdownItems({required this.text, required this.value});
+  DropdownItems({required this.text, required this.value, this.group});
 }
