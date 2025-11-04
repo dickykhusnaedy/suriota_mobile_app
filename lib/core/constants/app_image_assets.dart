dcd316eb8ef1ddcf01c26fa6b34506aea327fafc
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-11-04 21:46:06 +0700

    feat(assets): update images assets using .webp

--- lib/core/constants/app_image_assets.dart
+++ lib/core/constants/app_image_assets.dart
@@ -1,18 +1,18 @@
 class ImageAsset {
   ImageAsset._();
 
-  static const logoSuriota = 'assets/logo_suriota.png';
-  static const profile = 'assets/profile.jpg';
-  static const profile2 = 'assets/avatar.png';
-  static const iconBluetooth = 'assets/icon_bluetooth.png';
-  static const companyImage = 'assets/company_image.jpg';
-  static const iconGoogle = 'assets/google_icon.png';
-  static const iconModbus = 'assets/modbus_icon.png';
-  static const iconDevice = 'assets/communication_device.png';
-  static const iconLogging = 'assets/logging_config.png';
-  static const iconConfig = 'assets/config.png';
-  static const iconServer = 'assets/server.png';
-  static const iconDeviceInitial = 'assets/responsive.png';
-  static const suriotaGatewayBanner = 'assets/banner_suriota_gateway.png';
-  static const logoSuriotaAbout = 'assets/about-logo.jpg';
+  static const logoSuriota = 'assets/logo_suriota.webp';
+  static const profile = 'assets/profile.webp';
+  static const profile2 = 'assets/avatar.webp';
+  static const iconBluetooth = 'assets/icon_bluetooth.webp';
+  static const companyImage = 'assets/company_image.webp';
+  static const iconGoogle = 'assets/google_icon.webp';
+  static const iconModbus = 'assets/modbus_icon.webp';
+  static const iconDevice = 'assets/communication_device.webp';
+  static const iconLogging = 'assets/logging_config.webp';
+  static const iconConfig = 'assets/config.webp';
+  static const iconServer = 'assets/server.webp';
+  static const iconDeviceInitial = 'assets/responsive.webp';
+  static const suriotaGatewayBanner = 'assets/banner_suriota_gateway.webp';
+  static const logoSuriotaAbout = 'assets/about-logo.webp';
 }
